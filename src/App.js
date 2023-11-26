de59d4fda2e3ef0275cb3b2f746b79788cbadcbc
Žan <zan1303@hotmail.com>
2023-11-26 13:14:38 +0100

    updating logo and scss on navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import { Routes, Route, Outlet  } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
-
-const Navigation = () => {
-  return(
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet/>
-    </div>
-  )
-
-}
+import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => {
   return <h1> I am the shop page</h1>;
@@ -20,7 +9,7 @@ const Shop = () => {
 const App = () => {
   return (
   <Routes>
-    <Route path='/' element={Navigation}>
+    <Route path='/' element={<Navigation/>}>
       <Route index element={<Home />} />
       <Route path='shop' element={<Shop />} />
     </Route>
